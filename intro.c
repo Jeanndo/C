83aaf4cca32624fa5996b32dde65b149e213823c
Jeanndo <ukjeando@gmail.com>
2026-07-18 20:18:28 +0200

    introduction

---
+++ intro.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main() {
+    
+    int number =0;
+
+    printf("Enter a number: \n");
+
+    scanf("%d", &number);
+
+    printf("You entered: %d\n", number);
+
+    getchar();
+
+    return 0;
+}
